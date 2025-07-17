eede8d8553e751c3ea952a2fdf0132d960999716
Andrea Hernandez <andrea.hernandez@itti.digital>
2025-07-17 02:02:33 -0300

    feat: :sparkles: implement Screen of Routes

---
+++ core/navigation/src/main/java/com/example/navigation/Screen.kt
@@ -0,0 +1,18 @@
+package com.example.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class Screen(val route: String, val title: String, val icon: ImageVector) {
+    object Home : Screen("home", "Home", Icons.Default.Home)
+    object Profile : Screen("profile", "Profile", Icons.Default.Person)
+    object OrderHistory : Screen("order_history", "Order History", Icons.Default.List)
+    object OrderDetail : Screen("order/{orderId}", "Order Detail", Icons.Default.Info)
+    object OrderPay : Screen("order_payment", "Order Pay", Icons.Filled.ShoppingCart)
+    object Login : Screen("login", "", Icons.Default.AccountCircle)
+
+    companion object {
+        val all = listOf(Home, Profile, OrderHistory)
+    }
+}
