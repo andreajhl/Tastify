0939a54e25f4dba932ae23c28117cdd3a1c456fe
Andrea Hernandez <andrea.hernandez@itti.digital>
2025-07-18 03:05:08 -0300

    feat: :sparkles: add pagging global

---
+++ core/theme/src/main/java/com/example/theme/ui/theme/Styles.kt
@@ -0,0 +1,18 @@
+package com.example.theme.ui.theme
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+
+val TertiaryColor = Color(0xFF0E637E)
+val LightColor = Color(0xFFC8DAEA)
+val MainColor = Color(0xFF179ABB)
+
+
+val DisableTextColor = Color(0x808D8D8D)
+
+val DefaultScreenPadding = PaddingValues(
+    start = 24.dp,
+    end = 24.dp,
+    top = 16.dp
+)
